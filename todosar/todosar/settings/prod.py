3ca7c14b17cdeab906752bb4b9519257776d6dbd
David Moscoso <dfmoscoso23@gmail.com>
2026-07-13 17:20:23 +0200

    add session cookie domain

--- todosar/todosar/settings/prod.py
+++ todosar/todosar/settings/prod.py
@@ -1,4 +1,4 @@
-from .base import * # noqa: F403
+from .base import *  # noqa: F403
 import os
 
 DATABASES = {
@@ -22,16 +22,19 @@ ALLOWED_HOSTS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://api.todostuslibrosar.com.ar",
-    "https://todos.apiultragestion.com.ar", 
+    "https://todos.apiultragestion.com.ar",
+    "https://dev.todostuslibrosar.com.ar",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://todos.apiultragestion.com.ar",
+    "https://dev.todostuslibrosar.com.ar",
 ]
 
-SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = "None"
 SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SAMESITE = "None"
 CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_DOMAIN = ".todostuslibrosar.com.ar"
